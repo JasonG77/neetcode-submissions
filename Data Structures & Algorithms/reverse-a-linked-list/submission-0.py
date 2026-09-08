ddbf058e6c408ffd87aa840a78fdaa0641e26bc3
Jason Gutierrez <jasongutierrez316@gmail.com>
2026-09-08 12:46:49 -0500

    Update: reverse-a-linked-list - submission-0

--- Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
@@ -6,13 +6,25 @@
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
+        #Goal: Reverse the linked list
+        #Algorithm: Reassign arrows as we go down the linked list
+        
+        #[0 -> 1 -> 2 -> 3 -> None] -> [None <- 0 <- 1 <- 2 <- 3]
+        #[] - > []
+        #We reference both linked lists by their head list node
+        
+        #Keep track of the current node - starts at head
         curr = head
+        #keep track of the previous node and use it to reassign arrows
+        prev = None 
+        
+        #loop to keep track of when we have surpassed the linked list length
         while curr:
+            #keep track of next before we reassign (don't lose it!)
             nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
-        return prev
+            
+        return prev 
 
-        
