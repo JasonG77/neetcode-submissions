bdd58ccb24273ec1a14a11812fd9f02ea702af73
Jason Gutierrez <jasongutierrez316@gmail.com>
2026-09-10 02:32:33 -0500

    Update: binary-search - submission-0

--- Data Structures & Algorithms/binary-search/submission-0.py
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -7,15 +7,13 @@ class Solution:
         #4. repeat until we find target or until L and R poitners overlap
 
         l, r = 0, len(nums) - 1
-        while l <= r:
+
+        while l <= r: 
             midpoint = (l + r) // 2
-            if nums[midpoint] < target:
-                l = (midpoint + 1)
+            if nums[midpoint] < target: 
+                l = midpoint + 1
             elif nums[midpoint] > target:
-                r = (midpoint - 1)
+                r = midpoint - 1
             else: 
                 return midpoint
-        #didnt find the target - > l and R overlaped
         return -1 
-        
-        
